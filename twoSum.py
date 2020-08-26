af7b0031702013d59abe12202a4e3ee08512c0fb
gowthammg <35264050+gowthammg@users.noreply.github.com>
2020-08-26 20:07:39 +0530

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
-        h = {nums[i]:i for i in range(len(nums))}
-        #print(h)
         for i in range(len(nums)):
             x = nums[i]
             y = target - x
-            l = h.get(y)
-            if (i == l):
-                continue
-            if (l!=None):
-                break
-        return i,l
-            
+            if y in h:
+                return [h[y],i]
+            else:
+                h[nums[i]] = i
+        return []
